ec309d8f87fe5ebd7b968451d079037adc82bd22
Zhenzhong Zhou <bob.zhou@usask.ca>
2020-02-06 17:35:11 -0600

    Product Route Create/Index/New

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const router = express.Router();
+const Page =  require("../../models/page");
+const Category = require("../../models/category");
+const Product = require("../../models/product");
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+    let pages;
+    let categories;
+    let products;
+    try {
+        pages = await Page.find().sort({sorting: 1}).limit(10).exec();
+        categories = await Category.find({}).limit(10).exec();
+        products = await Product.find().sort({createdAt: "desc"}).limit(10).exec()
+    }catch (e) {
+        products = [];
+    }
     res.render("admin/index", {
+        pages: pages,
+        categories: categories,
+        products: products,
         login: "2"
     })
 });
