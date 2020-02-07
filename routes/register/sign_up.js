ae3e13e7168e144dc3a91ac95e133033e7de6fa5
Zhenzhong Zhou <bob.zhou@usask.ca>
2020-02-07 11:33:50 -0600

    fixed login part

--- routes/register/sign_up.js
+++ routes/register/sign_up.js
@@ -14,41 +14,74 @@ router.post("/", [
     check("password", "Password is required.").notEmpty().exists(),
     check("password2", "Passwords do not match.").exists()
         .custom((value, { req }) => value === req.body.password),
-], async (req, res) => {
+],  (req, res) => {
     const username = req.body.username;
     const email = req.body.email;
     const password = req.body.password;
     const password2 = req.body.password2;
 
-    const user = new User({
-        username: username,
-        email: email,
-        password: password
-    });
-    try {
-        // await user.createUser(user, function (err, user) {
-        //     if (err) throw err;
-        //     console.log(user);
-        // });
-        const newUser = await user.save();
+    // const user = new User({
+    //     username: username,
+    //     email: email,
+    //     password: password
+    // });
+    // user.createUser(user, function (err, user) {
+    //     if (err) throw err;
+    //     console.log(user);
+    // });
+    // // const newUser = await user.save();
+    // req.flash("success_msg", "You are registered and can now login.");
+    // console.log(user);
+    // res.redirect("/login");
+    // try {
+    //     await user.createUser(user, function (err, user) {
+    //         if (err) throw err;
+    //         console.log(user);
+    //     });
+    //     // const newUser = await user.save();
+    //     req.flash("success_msg", "You are registered and can now login.");
+    //     console.log(user);
+    //     res.redirect("/login");
+    // }catch (e) {
+    //     // validation
+    //     const errorFormatter = ({location, msg, param}) => {
+    //         return `${location}[${param}]: ${msg}`;
+    //     };
+    //     const results = validationResult(req).formatWith(errorFormatter);
+    //     if (!results.isEmpty()) {
+    //         res.render("register/sign_up", {
+    //             results: results,
+    //             login: "3"
+    //         })
+    //     }else {
+    //         console.log("PASSED")
+    //     }
+    // }
+    // validation
+    // const errorFormatter = ({location, msg, param}) => {
+    //     return `${location}[${param}]: ${msg}`;
+    // };
+    // const results = validationResult(req).formatWith(errorFormatter);
+    // if (!results.isEmpty()) {
+    //     res.render("register/sign_up", {
+    //         results: results,
+    //         login: "3"
+    //     })
+    // }else {
+        const user = new User({
+            username: username,
+            email: email,
+            password: password
+        });
+        User.createUser(user, function (err, user) {
+            if (err) throw err;
+            console.log(user);
+        });
+        // const newUser = await user.save();
         req.flash("success_msg", "You are registered and can now login.");
         console.log(user);
         res.redirect("/login");
-    }catch (e) {
-        // validation
-        const errorFormatter = ({location, msg, param}) => {
-            return `${location}[${param}]: ${msg}`;
-        };
-        const results = validationResult(req).formatWith(errorFormatter);
-        if (!results.isEmpty()) {
-            res.render("register/sign_up", {
-                results: results,
-                login: "3"
-            })
-        }else {
-            console.log("PASSED")
-        }
-    }
+    // }
 });
 
 module.exports = router;
