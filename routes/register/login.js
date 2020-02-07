511888b3107fc0b8c06092cdf42205e8426d5134
Zhenzhong Zhou <bob.zhou@usask.ca>
2020-02-07 10:46:55 -0600

    Add login part but not work

--- routes/register/login.js
+++ routes/register/login.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const router = express.Router();
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 const User = require("../../models/user");
 
 router.get("/", (req, res) => {
@@ -7,8 +9,39 @@ router.get("/", (req, res) => {
     res.render("register/login", {login: "3"});
 });
 
-router.post("/", (req, res) => {
+passport.use(new LocalStrategy(
+    function (username, password, done) {
+        User.getUserByUsername(username, function (err, user) {
+            if (err) throw err;
+            if (!user) return done(null ,false, {message: "Unknown User"});
+            User.comparePassword(password, user.password, function (err, isMatch) {
+                if (err) throw err;
+                if (isMatch) {
+                    return done(null, user)
+                }else {
+                    return done(null, false, {message: "Invalid password"})
+                }
+            })
+        })
+    }
+));
 
+passport.serializeUser((user, done) => {
+    done(null, user.id)
+});
+
+passport.deserializeUser((id, done) => {
+    User.getUserById(id, function (err, user) {
+        done(err, user);
+    })
+});
+
+router.post("/", passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "login",
+    failureFlash: true,
+}), (req, res) => {
+    res.redirect("/")
 });
 
 module.exports = router;
