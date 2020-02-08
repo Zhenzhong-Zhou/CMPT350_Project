efba8b470ea1a032bb4bc127e12b0b539082563f
Zhenzhong Zhou <bob.zhou@usask.ca>
2020-02-07 19:53:12 -0600

    add some validations

--- routes/register/sign_up.js
+++ routes/register/sign_up.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const bcrypt = require("bcryptjs");
 const User = require("../../models/user");
 const {forwardAuthenticated} = require("../../config/auth");
 const { check, validationResult } = require('express-validator');
@@ -13,28 +12,36 @@ router.post("/", [
     check("username", "Username is required.").notEmpty(),
     check("email", "Email is required.").notEmpty(),
     check("email", "Email is not valid.").isEmail(),
-    check("password", "Password is required.").notEmpty().isLength({min: 6}).exists(),
+    check("password", "Password must have at least 6 characters.").notEmpty().isLength({min: 6}).exists(),
     check("password2", "Passwords do not match.").exists()
         .custom((value, { req }) => value === req.body.password),
-],  (req, res) => {
+], async (req, res) => {
     const username = req.body.username;
     const email = req.body.email;
     const password = req.body.password;
-    const password2 = req.body.password2;
+
+    const errorFormatter = ({location, msg, param}) => {
+        return `${location}[${param}]: ${msg}`;
+    };
+    const result = validationResult(req).formatWith(errorFormatter);
+    if (!result.isEmpty()) {
+        return res.render("register/sign_up", {
+            errorMessage: result.array()[0],
+            errorMessage1: result.array()[1],
+            errorMessage2: result.array()[2],
+            login: "3"
+        });
+    }
 
     const user = new User({
         username: username,
         email: email,
-        password: password,
-        password2: password2
+        password: password
     });
-    User.createUser(user, function (err, user) {
+    User.createUser(user, function (err) {
         if (err) throw err;
-        console.log(user.username);
     });
-    // const newUser = await user.save();
     req.flash("success_msg", "You are registered and can now login.");
-    console.log(user.username);
     res.redirect("/login");
 });
 
