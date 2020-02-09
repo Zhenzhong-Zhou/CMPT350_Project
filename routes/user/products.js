abf4f0edd2bcee9a5ea46a004eca2f1c1d53b896
Zhenzhong Zhou <bob.zhou@usask.ca>
2020-02-09 00:44:10 -0600

    front display products by category but cannot load info

--- routes/user/products.js
+++ routes/user/products.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Product = require("../../models/product");
+const Category = require("../../models/category");
 const {isAdmin} = require("../../config/auth");
 const { check, validationResult } = require('express-validator');
 
@@ -8,18 +9,31 @@ const { check, validationResult } = require('express-validator');
  * GET All Products Route
  */
 router.get("/", (req, res) => {
-    console.log("111");
-    // res.send("hello")
-    // res.render("user/index")
-    // res.render("index")
     Product.find((err, products) => {
         if (err) console.log(err);
         res.render("user/products/all_products", {
             title: "All products",
             products: products,
             login: "1"
-        })
-    })
+        });
+    });
+});
+
+/*
+ * GET Products By Categories Route
+ */
+router.get("/:category", (req, res) => {
+    const categorySlug = req.params.category;
+    Category.findOne({slug: categorySlug}, (err, category) => {
+       Product.find({categoryName: categorySlug}, (err, products) => {
+            if (err) console.log(err);
+            res.render("user/products/category_products", {
+                title: category.categoryName,
+                products: products,
+                login: "1"
+            });
+        });
+    });
 });
 
 module.exports = router;
