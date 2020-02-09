8b6fa21ed93e6b954b48166d9ed70c96d9c654ad
Zhenzhong Zhou <bob.zhou@usask.ca>
2020-02-09 10:32:02 -0600

    TEST

--- routes/user/products.js
+++ routes/user/products.js
@@ -22,18 +22,45 @@ router.get("/", (req, res) => {
 /*
  * GET Products By Categories Route
  */
-router.get("/:category", (req, res) => {
+router.get("/:category", async (req, res) => {
     const categorySlug = req.params.category;
-    Category.findOne({slug: categorySlug}, (err, category) => {
-       Product.find({categoryName: categorySlug}, (err, products) => {
-            if (err) console.log(err);
-            res.render("user/products/category_products", {
-                title: category.categoryName,
-                products: products,
-                login: "1"
-            });
+    try {
+        const category = await Category.findOne({slug: categorySlug});
+        const products = await Product.find({categoryName: categorySlug}).exec();
+        res.render("user/products/category_products", {
+            title: category.categoryName,
+            products: products,
+            login: "1"
         });
-    });
+    }catch (e) {
+        res.redirect("/");
+    }
+    // Category.findOne({slug: categorySlug}, (err, category) => {
+    //     if (err) console.log(err);
+    //    Product.find({categoryName: categorySlug}, (err, products) => {
+    //         if (err) console.log(err);
+    //         res.render("user/products/category_products", {
+    //             title: category.categoryName,
+    //             products: products,
+    //             login: "1"
+    //         });
+    //     });
+    // });
+});
+
+/*
+ * GET Products By Categories Route
+ */
+router.get("/:category/:product", async (req, res) => {
+    try {
+        const product = await Product.findOne({slug: req.params.product}).exec();
+        res.render("user/products/product", {
+            product: product,
+            login: "1"
+        })
+    }catch (e) {
+        res.redirect("/");
+    }
 });
 
 module.exports = router;
