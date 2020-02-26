784487282ae9955d141ead0534fc2eb0d1b8af8f
Zhenzhong Zhou <bob.zhou@usask.ca>
2020-02-26 15:21:41 -0600

    Fixed BUGS HUGE IMPROVEMENT PERFERMANCE

--- routes/user/products.js
+++ routes/user/products.js
@@ -11,6 +11,7 @@ const { check, validationResult } = require('express-validator');
 router.get("/", (req, res) => {
     Product.find((err, products) => {
         if (err) console.log(err);
+        console.log(products);
         res.render("user/products/all_products", {
             title: "All products",
             products: products,
@@ -26,30 +27,15 @@ router.get("/:category", async (req, res) => {
     const categorySlug = req.params.category;
     try {
         const category = await Category.findOne({slug: categorySlug});
-        console.log(category);
-        // const products = await Product.find({productName: categorySlug}).exec();
-        const product = await Product.findById(req.params.id).populate("category").exec();
-        console.log(product);
+        const products = await Product.find({category: category._id}).exec();
         res.render("user/products/category_products", {
             title: category.categoryName,
-            product: product,
+            products: products,
             login: "1"
         });
     }catch (e) {
         res.redirect("/");
     }
-    // Category.findOne({slug: categorySlug}, (err, category) => {
-    //     if (err) console.log(err);
-    //    Product.find({categoryName: categorySlug}, (err, products) => {
-    //        console.log(products);
-    //         if (err) console.log(err);
-    //         res.render("user/products/category_products", {
-    //             title: category.categoryName,
-    //             products: products,
-    //             login: "1"
-    //         });
-    //     });
-    // });
 });
 
 /*
@@ -58,6 +44,7 @@ router.get("/:category", async (req, res) => {
 router.get("/:category/:product", async (req, res) => {
     try {
         const product = await Product.findOne({slug: req.params.product}).exec();
+        console.log(product)
         res.render("user/products/product", {
             product: product,
             login: "1"
