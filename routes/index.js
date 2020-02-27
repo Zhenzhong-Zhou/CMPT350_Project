469c52ee94cb7b815e4ee9854e3fa69c12ebb167
Zhenzhong Zhou <bob.zhou@usask.ca>
2020-02-27 13:21:41 -0600

    fix bugs

--- routes/index.js
+++ routes/index.js
@@ -11,35 +11,26 @@ router.get("/", async (req, res) => {
     try {
         const page = await Page.findOne({slug: "home"}).exec();
         res.render("index", {
-            title: page.title,
+            title: page.pageTitle,
             content: page.content,
             user: req.user,
             views: views,
             login: "1"
         })
     }catch (e) {
-        console.log(e);
+        res.redirect("/");
     }
-    // await Page.findOne({slug: "home"}, (err, page) => {
-    //     if (err) console.log(err);
-    //     res.render("index", {
-    //         title: page.pageTitle,
-    //         content: page.content,
-    //         user: req.user,
-    //         views: views,
-    //         login: "1"
-    //     })
-    // });
 });
 
 /*
  * GET user side page content
  */
-router.get("/:slug", (req, res) => {
+router.get("/:slug", async (req, res) => {
     const views = req.session.views++;
     const slug = req.params.slug;
-    Page.findOne({slug: slug}, (err, page) => {
-        if (err) console.log(err);
+    try {
+        const page = await Page.findOne({slug: slug}).exec();
+        console.log(page);
         if (!page) {
             res.redirect("/");
         }else {
@@ -49,9 +40,11 @@ router.get("/:slug", (req, res) => {
                 user: req.user,
                 views: views,
                 login: "1"
-            })
+            });
         }
-    });
+    }catch (e) {
+        res.redirect("/");
+    }
 });
 
 module.exports = router;
