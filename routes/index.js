4be778c60df6ae22081b77bfbcf661d6f39983e3
Zhenzhong Zhou <bob.zhou@usask.ca>
2020-02-27 16:33:06 -0600

    fix server bugs and admin page icon

--- routes/index.js
+++ routes/index.js
@@ -8,20 +8,18 @@ const {isUser} = require("../config/auth");
  */
 router.get("/", async (req, res) => {
     const views = req.session.views++;
-    res.redirect("/");
-    // try {
-    //     const page = await Page.findOne({slug: "home"}).exec();
-    //     res.render("index", {
-    //         title: page.pageTitle,
-    //         content: page.content,
-    //         user: req.user,
-    //         views: views,
-    //         login: "1"
-    //     })
-    // }catch (e) {
-    //     console.log(e);
-    //     res.redirect("/");
-    // }
+    try {
+        const page = await Page.findOne({slug: "home"}).exec();
+        res.render("index", {
+            title: page.pageTitle,
+            content: page.content,
+            user: req.user,
+            views: views,
+            login: "1"
+        })
+    }catch (e) {
+        res.redirect("/");
+    }
 });
 
 /*
@@ -45,8 +43,7 @@ router.get("/:slug", async (req, res) => {
             });
         }
     }catch (e) {
-        console.log(e);
-        // res.redirect("/");
+        res.redirect("/");
     }
 });
 
