6667380772220215daa9d90fe7c3e3abb488a18f
Zhenzhong Zhou <bob.zhou@usask.ca>
2020-02-29 15:22:19 -0600

    create a new seller

--- models/seller.js
+++ models/seller.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+
+const portraitBasePath = "upload/portrait";
 
 const sellerSchema = new mongoose.Schema({
-    sellerName: {
-        type: String,
-        required: true
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: "User"
     },
     gender: {
         type: String,
@@ -18,27 +20,13 @@ const sellerSchema = new mongoose.Schema({
         type: String,
         required: true
     },
-    password: {
-        type: String,
-        required: true
-    },
-    category: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: "Category"
-    },
     phoneNumber: {
-        type: Number,
+        type: String,
         required: true
     },
     address: {
         type: String,
         required: true
-    },
-    user: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true,
-        ref: "User"
     },
     seller: {
         type: Number,
@@ -46,29 +34,5 @@ const sellerSchema = new mongoose.Schema({
     }
 });
 
-const Seller = module.exports = mongoose.model("Seller", sellerSchema);
-
-module.exports.createSeller = function (newSeller, callback) {
-    bcrypt.genSalt(10, function(err, salt) {
-        bcrypt.hash(newSeller.password, salt, function (err, hash) {
-            newSeller.password = hash;
-            newSeller.save(callback);
-        });
-    });
-};
-
-module.exports.getSellerByName = function (sellerName, callback) {
-    const query = {sellerName: sellerName};
-    Seller.findOne(query, callback);
-};
-
-module.exports.getSellerById = function (id, callback) {
-    Seller.findById(id, callback);
-};
-
-module.exports.comparePassword = function (candidatePassword, hash, callback) {
-    bcrypt.compare(candidatePassword, hash, function (err, isMatch) {
-        if (err) throw err;
-        callback(null, isMatch);
-    });
-};
+module.exports = mongoose.model("Seller", sellerSchema);
+module.exports.portraitBasePath = portraitBasePath;
--- routes/user/sellers.js
+++ routes/user/sellers.js
@@ -1,12 +1,23 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../../models/user");
+const Seller = require("../../models/seller");
 const {isUser} = require("../../config/auth");
-const Category = require("../../models/category");
+const path = require("path");
+const uploadPath = path.join("public", Seller.portraitBasePath);
+const multer = require("multer");
+const imageMimeTypes = ["image/jpg", "image/jpeg", "image/png", "images/gif"];
+const upload = multer({
+   dest: uploadPath,
+    fileFilter: (req, file, callback) => {
+       callback(null, imageMimeTypes.includes(file.mimetype));
+    }
+});
 
 /*
  * GET Sellers Route
  */
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
     res.render("user/markets/index", {
         login: "1"
     })
@@ -16,16 +27,48 @@ router.get("/", (req, res) => {
  * GET New Seller Route
  */
 router.get("/new", async (req, res) => {
-    res.render("user/markets/new", {
-        login: "1"
-    })
+    await renderNewSeller(req, res, new Seller());
 });
 
 /*
  * POST Create Product Route
  */
-router.post("/", async (req, res) => {
-    res.send("Not finished Yet")
+router.post("/", upload.single("portrait"), async (req, res) => {
+    const fileName = req.file != null ? req.file.filename : null;
+    const seller = new Seller({
+        user: req.body.user,
+        gender: req.body.gender,
+        age: req.body.age,
+        phoneNumber: req.body.phoneNumber,
+        address: req.body.address,
+        portrait: fileName,
+        seller: 2
+    });
+    try {
+        const newSeller = await seller.save();
+        // res.redirect(`markets/sellers/${newSeller.id}`);
+        res.redirect("/markets/sellers");
+    }catch (e) {
+        console.log(e);
+        await renderNewSeller(req, res, seller, true);
+    }
 });
+
+async function renderNewSeller(req, res, seller, hasError = false) {
+    try {
+        const sellerName = req.user;
+        const user = await User.findOne({username: sellerName.username});
+        const params = {
+            user: user,
+            seller: seller,
+            login: "1"
+        };
+        if (hasError) params.errorMessage = "Wrong";
+        res.render("user/markets/new", params);
+    }catch (e) {
+        console.log(e);
+        res.redirect("/market/sellers")
+    }
+}
 
 module.exports = router;
