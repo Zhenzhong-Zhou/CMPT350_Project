454ce600ff1da82f9d3e49bbb42bc61b3c866f9e
Zhenzhong Zhou <bob.zhou@usask.ca>
2020-03-03 18:12:34 -0600

    find a bug: when user get in seller's product page, user info will overwrite to seller's info

--- routes/user/sellers.js
+++ routes/user/sellers.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../../models/user");
-const Seller = require("../../models/seller");
 const Product = require("../../models/product");
 const {isSeller} = require("../../config/auth");
 
