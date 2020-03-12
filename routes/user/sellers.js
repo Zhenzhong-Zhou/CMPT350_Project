849d750af37639894a714d4366c970a7aaae87f5
Zhenzhong Zhou <bob.zhou@usask.ca>
2020-03-12 12:34:18 -0600

    change seller route can search

--- routes/user/sellers.js
+++ routes/user/sellers.js
@@ -8,12 +8,24 @@ const {isSeller} = require("../../config/auth");
  * GET Sellers Route
  */
 router.get("/", isSeller, async (req, res) => {
+    let query = Product.find({});
+    let name = req.query.product_name;
+    let author = req.query.author;
+    if (name != null && name !== "") {
+        query = query.regex("productName", new RegExp(name, "i"));
+    }
+    if (author!= null && author !== "") {
+        query = query.regex("author", new RegExp(author, "i"));
+    }
     try {
         const user = await User.findOne({username: req.user.username});
         const users = await User.find({admin: 2});
-        const products = await Product.find({seller: user});
+        const seller_products = await Product.find({seller: user});
+        const products = await query.exec();
         res.render("user/sellers/index", {
+            seller_products: seller_products,
             products: products,
+            searchOptions: req.query,
             users: users,
             login: "1"
         });
@@ -26,12 +38,24 @@ router.get("/", isSeller, async (req, res) => {
  * GET Seller's Page Route
  */
 router.get("/:id", async (req, res) => {
+    let query = Product.find({});
+    let name = req.query.product_name;
+    let author = req.query.author;
+    if (name != null && name !== "") {
+        query = query.regex("productName", new RegExp(name, "i"));
+    }
+    if (author!= null && author !== "") {
+        query = query.regex("author", new RegExp(author, "i"));
+    }
     try {
-        const products = await Product.find({seller: req.params.id}).populate("seller").exec();
+        const seller_products = await Product.find({seller: req.params.id}).populate("seller").exec();
         const product = await Product.findOne({seller: req.params.id}).populate("seller").exec();
+        const products = await query.exec();
         res.render("user/sellers/show", {
             product: product,
+            seller_products: seller_products,
             products: products,
+            searchOptions: req.query,
             login: "1"
         });
     }catch (e) {
