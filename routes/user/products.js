e7cd7f9b56c64d0dc724e5d1b5154de5b12d53f1
Zhenzhong Zhou <bob.zhou@usask.ca>
2020-03-12 12:27:35 -0600

    add seller upload products css, change user header

--- routes/user/products.js
+++ routes/user/products.js
@@ -10,12 +10,24 @@ const { check, validationResult } = require('express-validator');
 /*
  * GET All Products Route
  */
-router.get("/", (req, res) => {
-    Product.find((err, products) => {
+router.get("/", async (req, res) => {
+    let query = Product.find({});
+    let name = req.query.product_name;
+    let author = req.query.author;
+    if (name != null && name !== "") {
+        query = query.regex("productName", new RegExp(name, "i"));
+    }
+    if (author!= null && author !== "") {
+        query = query.regex("author", new RegExp(author, "i"));
+    }
+    const products = await query.exec();
+    Product.find((err, all_products) => {
         if (err) console.log(err);
         res.render("user/products/all_products", {
             title: "All Products",
+            all_products: all_products,
             products: products,
+            searchOptions: req.query,
             login: "1"
         });
     });
@@ -25,13 +37,25 @@ router.get("/", (req, res) => {
  * GET Products By Categories Route
  */
 router.get("/:category", async (req, res) => {
+    let query = Product.find({});
+    let name = req.query.product_name;
+    let author = req.query.author;
+    if (name != null && name !== "") {
+        query = query.regex("productName", new RegExp(name, "i"));
+    }
+    if (author!= null && author !== "") {
+        query = query.regex("author", new RegExp(author, "i"));
+    }
     const categorySlug = req.params.category;
     try {
         const category = await Category.findOne({slug: categorySlug});
-        const products = await Product.find({category: category._id}).exec();
+        const category_products = await Product.find({category: category._id}).exec();
+        const products = await query.exec();
         res.render("user/products/category_products", {
             title: category.categoryName,
+            category_products: category_products,
             products: products,
+            searchOptions: req.query,
             login: "1"
         });
     }catch (e) {
@@ -43,7 +67,17 @@ router.get("/:category", async (req, res) => {
  * GET Products By Categories Route
  */
 router.get("/:category/:product", async (req, res) => {
+    let query = Product.find({});
+    let name = req.query.product_name;
+    let author = req.query.author;
+    if (name != null && name !== "") {
+        query = query.regex("productName", new RegExp(name, "i"));
+    }
+    if (author!= null && author !== "") {
+        query = query.regex("author", new RegExp(author, "i"));
+    }
     try {
+        const products = await query.exec();
         const product = await Product.findOne({slug: req.params.product}).populate("category").populate("seller").exec();
         const reviews = await Review.find({product: product}).populate("user").exec();
         const number = await Review.find({product: product}).countDocuments();
@@ -54,6 +88,8 @@ router.get("/:category/:product", async (req, res) => {
             reviews: reviews,
             number: number,
             replies: replies,
+            products: products,
+            searchOptions: req.query,
             login: "1"
         })
     }catch (e) {
